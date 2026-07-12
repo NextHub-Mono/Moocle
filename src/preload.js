2d0cb477503abe100ae82cd70c7db0db70ba180e
Mikancat10 <mikancat09@gmail.com>
2026-07-13 02:26:40 +0900

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,32 @@
-document.addEventListener("contextmenu", e => {
-  const tab = e.target.closest(".tab");
-  if (!tab) return;
+const { contextBridge, ipcRenderer } = require('electron');
 
-  e.preventDefault();
-  window.api.showTabMenu(tab.dataset.tabId);
+contextBridge.exposeInMainWorld('electronAPI', {
+  onTabsUpdated: (callback) => ipcRenderer.on('tabs-updated', (event, tabs, activeTabId) => callback(tabs, activeTabId)),
+  
+  navigate: (url) => ipcRenderer.invoke('navigate', url),
+  goBack: () => ipcRenderer.invoke('goBack'),
+  goForward: () => ipcRenderer.invoke('goForward'),
+  reload: () => ipcRenderer.invoke('reload'),
+  goHome: () => ipcRenderer.invoke('goHome'),
+  newTab: () => ipcRenderer.invoke('newTab'),
+  switchTab: (id) => ipcRenderer.invoke('switchTab', id),
+  closeTab: (id) => ipcRenderer.invoke('closeTab', id),
+  reorderTabs: (ids) => ipcRenderer.invoke('reorder-tabs', ids),
+  
+  setSidebarStatus: (isOpen) => ipcRenderer.invoke('set-sidebar-status', isOpen),
+  setBookmarkBarStatus: (isOpen) => ipcRenderer.invoke('set-bookmark-bar-status', isOpen),
+  
+  addBookmark: () => ipcRenderer.invoke('addBookmark'),
+  getBookmarks: () => ipcRenderer.invoke('getBookmarks'),
+  removeBookmark: (url) => ipcRenderer.invoke('removeBookmark'),
+  getHistory: () => ipcRenderer.invoke('getHistory'),
+  clearHistory: () => ipcRenderer.invoke('clearHistory'),
+
+  confirmDeleteBookmark: (title) => ipcRenderer.invoke('confirm-delete-bookmark', title),
+
+  openTabContextMenu: (tabId) => ipcRenderer.send('open-tab-context-menu', tabId),
+  openBrowserUiContextMenu: () => ipcRenderer.send('open-browser-ui-context-menu'),
+  minimize: () => ipcRenderer.send('window-minimize'),
+  maximize: () => ipcRenderer.send('window-maximize'),
+  close: () => ipcRenderer.send('window-close')
 });
